dc403ae38eaeedbc3eb1a1301b8b17ba4c2b4e33
Jason Chan <jlc370@gmail.com>
2016-05-24 18:04:58 -0400

    Implement editing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,6 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def show
-    @article=Article.find(params[:id])
-  end
-
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -24,7 +20,26 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+  
+  def show
+    @article=Article.find(params[:id])
+  end
+
   private
     def article_params
     #need top-lvl key, so article
